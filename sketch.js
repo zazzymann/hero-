992ffbe3bd09e88f943d1da61b30096f4c970248
zazzymann <90633631+zazzymann@users.noreply.github.com>
2021-11-25 08:47:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, box2, box3,box4,box5,box6,box7,box8,box9,box10;
+var hero,monster,rope,ground;
+
+function preload() {
+  bg = loadImage("gamingbackground2.png");
+}
+
+function setup() {
+  createCanvas(3000, 700);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600, 600, 1200, 20);
+
+  hero = new Hero(mouseX,mouseY,250);
+  rope = new Rope(hero.body, { x: 500, y: 50 });
+  monster = new Monster(1100,550,300);
+
+  box1 = new Box(600, 100, 70, 70);
+  box2 = new Box(900, 100, 70, 70);
+  box3 = new Box(900, 100, 70, 70);
+  box4 = new Box(900, 100, 70, 70);
+  box5 = new Box(900, 100, 70, 70);
+  box6 = new Box(900, 100, 70, 70);
+  box7 = new Box(900, 100, 70, 70);
+
+}
+
+function draw() {
+  background(bg);
+  Engine.update(engine);
+  ground.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+
+
+  hero.display();
+  monster.display();
+
+}
+
